09816af960f47c94a88751969380292414d8f50d
S-tanishi <tanichi.u.n.20@gmail.com>
2021-09-22 16:03:46 +0900

    add: VPC の設定追加

---
+++ main.tf
@@ -0,0 +1,23 @@
+// VPC の設定
+// provider はaws 専用ではなくGCPとかも使える
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+variable "app_name" {
+  default = "suzuki-0test-app"
+}  
+// variable 変数
+// default のIPアドレスを設定している
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+// aws_vpc に入れないといけないもんもがterraform に書かれている。IPとタグ
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = var.app_name
+  }
+}
