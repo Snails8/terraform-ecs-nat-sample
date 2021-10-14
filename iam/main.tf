1bec4d66774cde4fe8b5e354dd94f63e2c3e3ad9
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-14 18:26:24 +0900

    add: IAM role にアタッチする処理の作成とpolicy (log系)の作成

--- iam/main.tf
+++ iam/main.tf
@@ -15,6 +15,19 @@ resource "aws_iam_role" "task_execution" {
   assume_role_policy = file("./iam/task_execution_role.json")
 }
 
+# policy の追加 (Log 関連)
+resource "aws_iam_role_policy" "task_execution" {
+  role = aws_iam_role.task_execution.id
+  policy = file("./iam/task_execution_role_policy.json")=
+}
+
+# role にpolicy をattach するときに必要な設定
+resource "aws_iam_role_policy_attachment" "task_execution" {
+  role       = aws_iam_role.task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
 # ECS と関連付け
 output "iam_role_task_execution_arn" {
   value = aws_iam_role.task_execution.arn
