47a2b5efc98d1cecfbd7da58780504ac7cd6d264
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-14 16:55:28 +0900

    add: ELB 設定の作成とそのmodule化

---
+++ elb/main.tf
@@ -0,0 +1,89 @@
+# ===========================================================================
+# ELB の設定 AWS Elastic Load Balancing
+#
+# 受信したトラフィックを複数のアベイラビリティーゾーンの複数のターゲット (EC2 インスタンス、コンテナ、IP アドレスなど) に自動的に分散
+# 登録されているターゲットの状態をモニタリングし、正常なターゲットにのみトラフィックをルーティング
+# 使用すると負荷分散による障害耐性がつく
+
+# 、Application Load Balancer、Network Load Balancer、Gateway Load Balancer、Classic Load Balancer といったロードバランサーをサポート
+# ===========================================================================
+
+variable "app_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+# ========================================================================
+# LB
+# https://docs.aws.amazon.com/ja_jp/elasticloadbalancing/latest/application/introduction.html
+
+# ALB : Application Load Balancer
+# ========================================================================
+
+resource "aws_lb" "main" {
+  load_balancer_type = "application"
+  name               = var.app_name
+
+  security_groups = [aws_security_group.main.id]
+  # 対象のsubnet
+  subnets = var.public_subnet_ids
+}
+
+# Security Group
+resource "aws_security_group" "main" {
+  name        = "${var.app_name}-alb"
+  description = "${var.app_name}-alb"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.app_name}-alb"
+  }
+}
+
+resource "aws_security_group_rule" "http" {
+  security_group_id = aws_security_group.main.id
+
+  type = "ingress"
+
+  from_port = 80
+  to_port   = 80
+  protocol = "tcp"
+
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      status_code  = "200"
+      message_body = "ok"
+    }
+  }
+}
+
+# ロードバランサーがリクエストを受け渡すルール
+output "http_listener_arn" {
+  value = aws_lb_listener.http.arn
+}
--- main.tf
+++ main.tf
@@ -50,6 +50,13 @@ module "ecs_cluster" {
   app_name = var.app_name
 }
 
+# ELB の設定
+module "elb" {
+  source = "./elb"
+  app_name = var.app_name
+  vpc_id = module.network.vpc_id
+  public_subnet_ids = module.network.public_subnet_ids
+}
 
 # ========================================================
 # RDS 作成
