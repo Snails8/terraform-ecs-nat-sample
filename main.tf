cfcda1c9c9e310ad6a94525bd46711c7f5a6b3c9
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-21 12:06:20 +0900

    refactor: variablesのリファクタリング

--- main.tf
+++ main.tf
@@ -20,42 +20,6 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-variable "app_name" {
-  type = string
-  default = "sample"
-}
-
-# AZ の設定(冗長化のため配列でlist化してある)
-variable "azs" {
-  type = list(string)
-  default = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
-}
-
-# ELB で使用 https化に使う
-variable "domain" {
-  type = string
-  default = "sample.com"
-}
-
-# acm で使用 (TLS証明書)
-variable "zone" {
-  type = string
-  default = "sample.com"
-}
-
-variable "LOKI_USER" {
-  type = string
-}
-
-variable "LOKI_PASS" {
-  type = string
-}
-
-variable "test_enviroment" {
-  default = true
-  # default = false
-}
-
 # ========================================================
 # Network 作成
 #
@@ -138,19 +102,6 @@ module "iam" {
 #
 # [subnetGroup, securityGroup, RDS instance(postgreSQL)]
 # ========================================================
-
-variable "DB_NAME" {
-  type = string
-}
-
-variable "DB_MASTER_NAME" {
-  type = string
-}
-
-variable "DB_MASTER_PASS" {
-  type = string
-}
-
 # RDS (PostgreSQL)
 module "rds" {
   source = "./rds"
