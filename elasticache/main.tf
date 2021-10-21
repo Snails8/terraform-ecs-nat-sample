8f34a74d88e452f514fcb6804b73bf13045330aa
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-21 12:35:12 +0900

    refactor: 責務の分離と可読性を上げるためにインデント調整

--- elasticache/main.tf
+++ elasticache/main.tf
@@ -1,33 +1,20 @@
-variable "app_name" {
-  type = string
-}
-
-variable "vpc_id" {
-  type = string
-}
-
-variable "private_subnet_ids" {
-  type = list(string)
-}
-
 locals {
   name = "${var.app_name}-redis"
 }
 
 # ========================================================
 # Elasticache (Redis)
-# 
 # ========================================================
 resource "aws_elasticache_cluster" "main" {
-  cluster_id = var.app_name
-  subnet_group_name = aws_elasticache_subnet_group.main.name
+  cluster_id         = var.app_name
+  subnet_group_name  = aws_elasticache_subnet_group.main.name
   security_group_ids = [aws_security_group.main.id]
 
-  engine = "redis"
-  engine_version = "5.0.6"
-  port = 6379
+  engine          = "redis"
+  engine_version  = "5.0.6"
+  port            = 6379
   parameter_group_name = "default.redis5.0"
-  node_type = "cache.t3.micro"
+  node_type       = "cache.t3.micro"
   num_cache_nodes = 1
 }
 
@@ -62,7 +49,7 @@ resource "aws_security_group_rule" "redis" {
 }
 
 resource "aws_elasticache_subnet_group" "main" {
-  name = local.name
+  name        = local.name
   description = local.name
-  subnet_ids = var.private_subnet_ids
+  subnet_ids  = var.private_subnet_ids
 }
