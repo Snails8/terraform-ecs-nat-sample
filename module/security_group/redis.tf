d9c8d29bae5eba3f672411e4d4dd1677ee839390
S-tanishi <tanichi.u.n.20@gmail.com>
2022-01-05 11:51:47 +0900

    refactor: ディレクトリ変更における参照先に問題が発生していたのでその修正

--- module/security_group/redis.tf
+++ module/security_group/redis.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "redis_ecs" {
 
   # aws_security_group_rule
   ingress {
-    cidr_blocks = ["0.0.0.0/0"]∑
+    cidr_blocks = ["0.0.0.0/0"]
     from_port   = 6379
     to_port     = 6379
     protocol    = "tcp"
